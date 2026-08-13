bdcb54a57443f8a419e4b4d1590c7fda7a62464b
lokeshbisht915 <158795443+lokeshbisht915@users.noreply.github.com>
2026-08-13 03:25:53 +0000

    code completed

--- regular_falsi.c
+++ regular_falsi.c
@@ -5,28 +5,28 @@ double fun(double x){
 }
 
 int main(){
-    double x1, x2, x3;
+    double x0, x1, x3;
     int counter;
 
     printf("Enter the upper and lower interval x1 and x2\nwhere the function return opposite sign");
-    scanf("%lf %lf",&x1,&x2);
+    scanf("%lf %lf",&x0,&x1);
 
     printf("Enter the no iteration you want to perform ");
     scanf("%d",&counter);
     
     
-    if((fun(x1)*fun(x2))<0){
-        printf("1");
+    if((fun(x0)*fun(x1))<0){
+        // printf("%lf '*' %lf = %lf\n",fun(x0),fun(x1),(fun(x0)*fun(x1)));
         int i = 1;
         while(i<=counter){
-            
-        x3 = (x1 + x2)/2;
-        printf("Interval: %d the lower limit: %lf upper limit: %lf Approxiation: %lf result: %lf\n ",i,x1,x2,x3,fun(x3));
+        printf("%lf '*' %lf = %lf\n",fun(x0),fun(x1),(fun(x0)*fun(x1)));    
+        x3 = (x0 - (((x1 - x0) * fun(x0))/(fun(x1)-fun(x0))));
+        printf("Interval: %d the lower limit: %lf upper limit: %lf Approxiation: %lf result: %lf\n ",i,x0,x1,x3,fun(x3));
         
         if((fun(x3)<0)){
-            x1 = x3;
+            x0 = x3;
         }else{
-            x2 = x3;
+            x1 = x3;
         }
         i++;
         }
